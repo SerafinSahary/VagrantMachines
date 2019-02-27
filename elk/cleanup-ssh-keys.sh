8f8b566651d91e26c5a872612090424fc706405d
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2019-02-28 00:07:41 +0100

    SSH cleanup script improvement

--- elk/cleanup-ssh-keys.sh
+++ elk/cleanup-ssh-keys.sh
@@ -1,5 +1,10 @@
 ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2200
 ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2222
+ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R 192.168.33.10
+ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R 192.168.33.11
 
-ssh -N -p 2222 vagrant@127.0.0.1
-ssh -N -p 2200 vagrant@127.0.0.1
+ssh -p 2222 vagrant@127.0.0.1 /bin/true
+ssh -p 2200 vagrant@127.0.0.1 /bin/true
+ssh vagrant@192.168.33.10 /bin/true
+ssh vagrant@192.168.33.11 /bin/true
+
