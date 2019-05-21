5bba3b9b7b6cc18166a8657bc00148e7b2f14507
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2019-05-21 23:37:28 +0200

    - new role for ElastAlert kibana plugin - finetuning package installation - packages installation status latest -> present

--- elk/cleanup-ssh-keys.sh
+++ elk/cleanup-ssh-keys.sh
@@ -1,10 +1,16 @@
-ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2200
 ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2222
+ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2200
+ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R [127.0.0.1]:2201
+
 ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R 192.168.33.10
 ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R 192.168.33.11
+ssh-keygen -f "/home/serafin/.ssh/known_hosts" -R 192.168.33.12
 
 ssh -p 2222 vagrant@127.0.0.1 /bin/true
 ssh -p 2200 vagrant@127.0.0.1 /bin/true
+ssh -p 2201 vagrant@127.0.0.1 /bin/true
+
 ssh vagrant@192.168.33.10 /bin/true
 ssh vagrant@192.168.33.11 /bin/true
+ssh vagrant@192.168.33.12 /bin/true
 
--- masterconfig/create-role-dirs.sh
+++ masterconfig/create-role-dirs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -vp $1/{tasks,handlers,files,templates,vars,defaults,meta}
+mkdir -p $1/{tasks,handlers,files,templates,vars,defaults,meta}
 
 for i in `ls -1 $1`; do touch $1/$i/README.md; done
+
+ls -lR $1
 
